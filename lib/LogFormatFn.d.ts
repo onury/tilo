52432146daef96047546979ead49515684c749d7
Onur Yıldırım <onur@cutepilot.com>
2019-01-28 04:22:36 +0300

    build

--- lib/LogFormatFn.d.ts
+++ lib/LogFormatFn.d.ts
@@ -3,7 +3,8 @@ import { ILogInfo } from './';
 /**
  *  Log format function.
  *  @param {ILogInfo} info - Log information object.
- *  @param {Chalk} chalk - Chalk instance for styling and coloring the output.
+ *  @param {Chalk} chalk - {@link https://github.com/chalk/chalk|Chalk} instance
+ *  for styling and coloring the output.
  *  @returns {string}
  */
 declare type LogFormatFn = (info: ILogInfo, chalk: Chalk) => string;
